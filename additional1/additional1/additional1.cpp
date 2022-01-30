932f5149bd97e80ddf2ae2cb64c14ad9bbb5c122
Sofya Shirokaya <tolkieniel@gmail.com>
2022-01-30 17:37:43 +0200

    problem 1 solved

---
+++ additional1/additional1/additional1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IfPalindrom(string word) {
+	if (word.length() <= 1) return 1;
+	string shortword = word.substr(1, word.length() - 2);
+	if ((word[0] == word[word.length() - 1]) && (IfPalindrom(shortword))) {
+		return 1;
+	}
+	else return 0;
+}
+
+int main()
+{
+	string word;
+	cin >> word;
+	cout << IfPalindrom(word);
+	return 0;
+}
